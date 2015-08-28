e3cf3802cb1d3dfc6c529124f00c21c0829d31ea
Jason White <jason.white@shopify.com>
2015-08-28 12:41:10 -0400

    Accelerometer

--- App/Direction.js
+++ App/Direction.js
@@ -3,14 +3,37 @@ var {
   Component,
   StyleSheet,
   View,
-  Text
+  Text,
+  DeviceEventEmitter
 } = React
+var {
+  Accelerometer
+} = require('NativeModules');
+
 
 class Direction extends Component {
+  constructor() {
+    super()
+    this.state = {x: 0, y: 0, z: 0}
+  }
+
+  componentDidMount() {
+    Accelerometer.setAccelerometerUpdateInterval(0.1) // in seconds
+
+    DeviceEventEmitter.addListener('AccelerationData', function (data) {
+      this.state.x = data.acceleration.x
+      this.state.y = data.acceleration.y
+      this.state.z = data.acceleration.z
+    })
+    Accelerometer.startAccelerometerUpdates() // you'll start getting AccelerationData events above
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Direction Page!</Text>
+        <Text style={styles.text}>x: {this.state.x}</Text>
+        <Text style={styles.text}>y: {this.state.y}</Text>
+        <Text style={styles.text}>z: {this.state.z}</Text>
       </View>
     );
   }
